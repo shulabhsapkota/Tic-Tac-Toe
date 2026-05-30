3875f104e004b40db4e5d7625b7fc51422e2155f
shulabhsapkota <shulabhsapkota12@gmail.com>
2026-05-30 14:31:14 +0545

    win detection

--- game.js
+++ game.js
@@ -1,10 +1,35 @@
 let boxes = document.querySelectorAll(".box");
 let turnO = true;
 
+const winPatterns = [
+  [0,1,2],[0,3,6],[0,4,8],
+  [1,4,7],[2,5,8],
+  [2,4,6],[3,4,5],[6,7,8]
+];
+
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
     box.innerText = turnO ? "O" : "X";
     turnO = !turnO;
     box.disabled = true;
+
+    checkWinner();
   });
 });
+
+function checkWinner() {
+  for (let pattern of winPatterns) {
+    let a = boxes[pattern[0]].innerText;
+    let b = boxes[pattern[1]].innerText;
+    let c = boxes[pattern[2]].innerText;
+
+    if (a && a === b && b === c) {
+      alert(`${a} wins!`);
+      disableAll();
+    }
+  }
+}
+
+function disableAll() {
+  boxes.forEach(box => box.disabled = true);
+}
